e6ffcee5ce057b317932985a7469899d8e04cc8f
bbarraza-axteroid <bbarraza@axteroid.com>
2025-09-05 16:08:54 -0400

    Update __manifest__.py

--- cl_bice_nomina_export/__manifest__.py
+++ cl_bice_nomina_export/__manifest__.py
@@ -1,16 +1,14 @@
-# cl_bice_nomina_export/__manifest__.py
 {
-    "name": "CL: Exportador Nómina Banco BICE (Proveedores)",
-    "version": "17.0.1.0.0",
-    "author": "Beagle",
-    "category": "Accounting",
-    "depends": ["account", "account_batch_payment"],
-    "data": [
-        # PRIMERO: acción + vista del wizard
-        "wizard/bice_export_wizard_views.xml",
-        # DESPUÉS: herencia de la vista de lotes con el botón
-        "views/account_batch_payment_views.xml",
-        # (si tienes security/ir.model.access.csv déjalo aquí también)
+    'name': 'Exportador BICE Nómina Proveedores',
+    'version': '1.0',
+    'summary': 'Exportación de nómina proveedores Banco BICE desde pagos por lote',
+    'category': 'Accounting',
+    'author': 'Branco Barraza',
+    'depends': ['account_batch_payment'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_batch_payment_views.xml',
+        'views/bice_export_wizard_views.xml',
     ],
-    "installable": True,
+    'installable': True,
 }
