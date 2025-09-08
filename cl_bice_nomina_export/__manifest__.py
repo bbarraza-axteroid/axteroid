96ef8aa0d181ae878b1ab79b574e392bf7c9e3c3
bbarraza-axteroid <bbarraza@axteroid.com>
2025-09-08 12:08:58 -0300

    Update __manifest__.py

--- cl_bice_nomina_export/__manifest__.py
+++ cl_bice_nomina_export/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "CL: Exportador Nómina Banco BICE (Proveedores)",
     "version": "17.0.2.1.0",
     "summary": "Genera archivo CSV (layout BICE Proveedores) desde Pagos en Lote (Tipo cuenta fijo Corriente).",
-    "author": "BrancoBarraza",
+    "author": "Branco Barraza",
     "website": "",
     "license": "LGPL-3",
     "depends": ["account"],
